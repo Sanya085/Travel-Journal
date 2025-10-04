fc43f5b8a23304f7bb0f6421f54100d5273dd430
Sanya <sanyapandey080805@gmail.com>
2025-10-04 10:22:23 +0530

    Travel Journal Project

---
+++ travel-project/src/components/Entry.jsx
@@ -0,0 +1,22 @@
+export default function Entry(props){
+    console.log(props)
+    return(
+        <article className="journal">
+            <div className="main-image-container">
+            <img 
+                 className="main-image" 
+                 src={props.entry.img.src}
+                 alt={props.entry.img.alt}
+                 />
+            </div>
+            <div className="info-container">
+                <img className="marker" src="/marker.png" alt="app marker icon" />
+                <span className="country">{props.entry.country}</span>
+                <a href={props.entry.googleMapsLink}>View on google Map</a>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="trip-dates">{props.entry.dates}</p>
+                <p className="entry-text">{props.entry.text}</p>
+            </div>
+        </article>
+    )
+}
